08b998ce8425ddc0cb9ef376022038b4d8802901
P-Chan <hello@0x50.io>
2022-11-25 15:58:09 +0900

    feat: コンポーネントを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,38 @@
-const Component = () => {
-  return <h1>Messages</h1>
+import { useEffect, useState } from 'react'
+import { MessageModel, createMessages } from '../models/message'
+
+type Props = {
+  messages: MessageModel[]
 }
 
-export default Component
+const Component = ({ messages }: Props) => {
+  return (
+    <div>
+      <h1>Messages</h1>
+      <ul>
+        {messages.map(({ id, body }) => {
+          return <li key={id}>{body}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export const Container = () => {
+  const [messages, setMessages] = useState<MessageModel[]>([])
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await fetch('/api/messages')
+      const json = await response.json()
+
+      const typedJSON = createMessages(json.messages)
+
+      setMessages(typedJSON)
+    })()
+  }, [])
+
+  return <Component messages={messages} />
+}
+
+export default Container
