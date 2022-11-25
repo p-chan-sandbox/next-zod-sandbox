ab0d177bc20b412da111ac60e6101dcede2d36be
P-Chan <hello@0x50.io>
2022-11-25 15:57:48 +0900

    feat: preprocess を追加

--- src/models/message.ts
+++ src/models/message.ts
@@ -2,13 +2,24 @@ import { z } from 'zod'
 
 import { userSchema } from './user'
 
-const messageSchema = z.object({
-  id: z.string(),
-  user: userSchema,
-  body: z.string(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-})
+const messageSchema = z.preprocess(
+  (json: any) => {
+    return {
+      id: json.id,
+      user: json.user,
+      body: json.body,
+      createdAt: json.created_at,
+      updatedAt: json.updated_at,
+    }
+  },
+  z.object({
+    id: z.string(),
+    user: userSchema,
+    body: z.string(),
+    createdAt: z.string(),
+    updatedAt: z.string(),
+  })
+)
 
 export type MessageModel = z.infer<typeof messageSchema>
 
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,21 @@
 import { z } from 'zod'
 
-export const userSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  gender: z.union([z.literal('male'), z.literal('female')]).optional(),
-  thumbnailImageURL: z.string().optional(),
-})
+export const userSchema = z.preprocess(
+  (json: any) => {
+    return {
+      id: json.id,
+      name: json.name,
+      gender: json.gender ?? undefined,
+      thumbnailImageURL: json.thumbnail_image_url ?? undefined,
+    }
+  },
+  z.object({
+    id: z.string(),
+    name: z.string(),
+    gender: z.union([z.literal('male'), z.literal('female')]).optional(),
+    thumbnailImageURL: z.string().optional(),
+  })
+)
 
 export type UserModel = z.infer<typeof userSchema>
 
